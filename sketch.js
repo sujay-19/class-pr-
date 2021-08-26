3d565032a6b8f02194d020956883c461c3903499
sujay-19 <85878065+sujay-19@users.noreply.github.com>
2021-08-26 15:54:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box,box1,box2;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  box1 = new Rectangle(200,200,50,20)
+}
+
+function draw() {
+  background("black");  
+  
+  box1.show()
+}
